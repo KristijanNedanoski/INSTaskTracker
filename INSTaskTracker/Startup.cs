04914c48180538a53c2b4658fdf66d21c6f5388d
Kristijan Nedanoski <kristijannedanoski@gmail.com>
2019-01-21 11:37:01 +0100

    First commit

---
+++ INSTaskTracker/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INSTaskTracker.Startup))]
+namespace INSTaskTracker
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
