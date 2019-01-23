209d9059c1fc2c392bea5af98560cd0cce23e2d3
Kristijan Nedanoski <kristijannedanoski@gmail.com>
2019-01-23 11:45:52 +0100

    Fifth commit. Database created, roles created and administrator added.

--- INSTaskTracker/Global.asax.cs
+++ INSTaskTracker/Global.asax.cs
@@ -8,6 +8,7 @@ using System.Web.Security;
 using System.Web.SessionState;
 using System.Data.Entity;
 using INSTaskTracker.Models;
+using INSTaskTracker.Logic;
 
 namespace INSTaskTracker
 {
@@ -19,7 +20,18 @@ namespace INSTaskTracker
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+            // Initialize the product database.
             Database.SetInitializer(new ProjectAssignmentDatabaseInitializer());
+
+            // Create the administrator role and user.
+            RoleActions roleActions = new RoleActions();
+            roleActions.CreateAdmin();
+
+            // Create the developer and client roles
+            RoleActions Developer = new RoleActions();
+            Developer.CreateDeveloper();
+            RoleActions Client = new RoleActions();
+            Client.CreateClient();
         }
     }
 }
